97c6b3fb3bcdca8946a376cbb0c5daf6f303f1e4
Rupesh Karale <rupeshkarale3245@gmail.com>
2023-07-07 21:24:27 +0530

    typeorm added

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,19 +10,23 @@ import { UserController } from './user/user.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtService } from '@nestjs/jwt/dist/jwt.service';
 import { ConfigModule } from '@nestjs/config';
+import { typeOrmAsyncConfig } from './config/typeorm-config';
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: (process.env.PORT as any) || 'mysql',
-      host: process.env.localhost as any,
-      port: parseInt(process.env.DB_PORT as string, 10) || 3306,
-      username: process.env.username,
-      password: process.env.password,
-      database: 'blog',
-      entities: ['dist/**/*.entity.js'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
+    // TypeOrmModule.forRoot({
+    //   type: (process.env.PORT as any) || 'mysql',
+    //   host: process.env.localhost as any,
+    //   port: parseInt(process.env.DB_PORT as string, 10) || 3306,
+    //   username: process.env.username,
+    //   password: process.env.password,
+    //   database: 'blog',
+    //   entities: ['dist/**/*.entity.js'],
+    //   synchronize: true,
+    // }),
     JwtModule.register({
       global: true,
       secret: 'your-secret',
